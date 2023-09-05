8d607d1558b5d0c216ee293d03692827655c2af0
Antonis Angelakis <mail@angelakis.eu>
2023-09-05 18:08:13 +0200

    Solve DNA

---
+++ dna.py
@@ -0,0 +1,12 @@
+def count_dna_bases(dna):
+    thymines = dna.count("T")
+    adenines = dna.count("A")
+    guanines = dna.count("G")
+    cytosines = dna.count("C")
+    return(adenines, cytosines, guanines, thymines)
+
+if __name__ == "__main__":
+    with open("rosalind_dna.txt") as f:
+        dna = f.readline().strip()
+    adenines, cytosines, guanines, thymines = count_dna_bases(dna)
+    print(f"{adenines} {cytosines} {guanines} {thymines}")
