f609bb626e63a217d3a65f75d6706e96914ea978
Antonis Angelakis <mail@angelakis.eu>
2023-09-11 18:06:16 +0200

    Solve PROB

---
+++ prob.py
@@ -0,0 +1,22 @@
+from math import log
+
+def logs_of_prob_gc(dna, gcProbs):
+    probs = []
+    for prob in gcProbs:
+        cumul_prob = log(1.0, 10)
+        base_probs = {
+            "C": prob/2,
+            "G": prob/2,
+            "A": (1-prob)/2,
+            "T": (1-prob)/2,
+        }
+        for base in dna:
+            cumul_prob += log(base_probs[base], 10)
+        probs.append(cumul_prob)
+    return(probs)
+
+if __name__ == "__main__":
+    with open("rosalind_prob.txt") as f:
+        dna = f.readline().strip()
+        gcProbs = list(map(float, f.readline().split()))
+    print(" ".join(map(str, logs_of_prob_gc(dna, gcProbs))))
