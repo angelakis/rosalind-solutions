62606de7904e960606d62c7dce4937146cc641d9
Antonis Angelakis <mail@angelakis.eu>
2023-09-15 02:23:06 +0200

    Solve ASPC

---
+++ aspc.py
@@ -0,0 +1,9 @@
+import math
+
+if __name__ == "__main__":
+    with open("rosalind_aspc.txt") as f:
+        n, m = [int(x) for x in f.readline().strip().split()]
+    ss = 0
+    for i in range(m, n+1):
+        ss = (ss + math.comb(n, i)) % 1000000
+    print(ss)
