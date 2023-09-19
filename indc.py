f4518da9c0dc87f889b632653a9f161ba6e67029
Antonis Angelakis <mail@angelakis.eu>
2023-09-19 17:51:00 +0200

    Solve INDC

---
+++ indc.py
@@ -0,0 +1,17 @@
+from math import log, factorial
+from decimal import Decimal
+from fractions import Fraction
+
+
+def calculate_prob_for_common_chromosomes(n):
+    probs = []
+    prob_sum = Fraction(1, 1)
+    for i in range(1, n+1):
+        prob_sum -= Fraction(factorial(n), factorial(i-1) * factorial(n-i+1)) * Fraction(1, 2**n)
+        probs.append(round(Decimal(prob_sum.numerator).log10()-Decimal(prob_sum.denominator).log10(), 3) + 0)
+    return probs
+
+if __name__ == "__main__":
+    with open("rosalind_indc.txt") as f:
+        n = int(f.readline().strip())
+    print(" ".join(map(str, calculate_prob_for_common_chromosomes(2*n))))
