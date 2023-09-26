b66fd128fe33a1d89d0c69b387ee5946f169f7c1
Antonis Angelakis <mail@angelakis.eu>
2023-09-26 19:13:00 +0200

    Solve SETO

---
+++ seto.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    with open("rosalind_seto.txt") as f:
+        n = int(f.readline().strip())
+        setA  = eval(f.readline().strip())
+        setB  = eval(f.readline().strip())
+    fullSet = set(range(1, n+1))
+    print(setA | setB)
+    print(setA & setB)
+    print(setA - setB)
+    print(setB - setA)
+    print(fullSet - setA)
+    print(fullSet - setB)
