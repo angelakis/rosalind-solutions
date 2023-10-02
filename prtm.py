75bcd35a6c5b28dee3328042f491eb0a4701f0eb
Antonis Angelakis <mail@angelakis.eu>
2023-10-02 21:17:01 +0200

    Solve PRTM

---
+++ prtm.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+
+from rosa_tools import aaMonoisotopeMass
+
+def calculate_prot_weight(prot):
+    weight = sum(Decimal(aaMonoisotopeMass[aa]) for aa in prot)
+    return(weight)
+
+if __name__ == "__main__":
+    with open("rosalind_prtm.txt") as f:
+        prot = f.readline().strip()
+    print(calculate_prot_weight(prot))
