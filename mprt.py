1670049b3b77047bc26511a56a4875a75ef1deaa
Antonis Angelakis <mail@angelakis.eu>
2023-10-04 18:17:11 +0200

    Finish MPRT

--- mprt.py
+++ mprt.py
@@ -1,17 +1,42 @@
+from re import finditer
+
 import requests
+
 
 def get_prot_sequences(prots):
     seqs = []
     for prot in prots:
-        r = requests.get(f"http://www.uniprot.org/uniprot/{prot}.fasta")
-        breakpoint()
-        seqs.append(r.text.split('\n')[1])
+        protId = prot.split('_')[0]
+        r = requests.get(f"http://www.uniprot.org/uniprot/{protId}.fasta")
+        r.raise_for_status()
+        seqs.append("".join(r.text.split('\n')[1:]))
     return(seqs)
 
+# Can't use this because we need also overlapping
+# def get_locations_by_regex(regex_pattern, text):
+#     locs = []
+#     for match in finditer(regex_pattern, text):
+#         locs.append(match.span()[0] + 1)
+#     return(locs)
 
+def get_nGlycosylation_locations(protSeq):
+    locs = []
+    for i in range(len(protSeq)-3):
+        if (
+            protSeq[i] == 'N'
+            and protSeq[i+1] != 'P'
+            and protSeq[i+2] in ('S', 'T')
+            and protSeq[i+3] != 'P'
+        ):
+            locs.append(i + 1)
+    return(locs)
 
 if __name__ == "__main__":
     with open("rosalind_mprt.txt") as f:
         prots = [prot.strip() for prot in f.readlines()]
     protSequences = get_prot_sequences(prots)
-    # print(round(calculate_dominant_phenotype(k, m, n), 5))
+    for i, protSeq in enumerate(protSequences):
+        locs = get_nGlycosylation_locations(protSeq)
+        if locs:
+            print(prots[i])
+            print(" ".join(map(str, locs)))
